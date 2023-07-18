38ba8b61ef69824f03deba520d7bad1241c72bf4
George Kinyanjui <kinyanjuigeorge827@gmail.com>
2023-07-18 21:22:11 +0300

    Add pricing text

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import "../style/pricing.css"
 
 function Pricing() {
   return (
     <div>
-      <h1>pricing</h1>
+      <h1>pricing under contruction..</h1>
+      <p className="soon">coming soon</p>
     </div>
   )
 }
