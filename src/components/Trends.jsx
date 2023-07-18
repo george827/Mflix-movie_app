3908af32dbcf081643c1748b6f4fed418ff93e0f
George Kinyanjui <kinyanjuigeorge827@gmail.com>
2023-07-18 13:43:33 +0300

    fetch trends data

--- src/components/Trends.jsx
+++ src/components/Trends.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
 
 function Trends() {
+  const Api = `https://api.themoviedb.org/3`
+  const TrendsShown =  "/trending/all/week";
+  const [TrendsArray, setTrendsArray] = useState([])
+  const Trends = async() => {
+    const data = await axios.get(`${Api}${TrendsShown}`, {
+      params: {
+        api_key: "09543633934773fe0f7b19a738612ce3",
+      }
+    })
+    const res = data.data.results;
+    setTrendsArray(res)
+  }
+
+  useEffect(() => {
+    Trends()
+  }, [])
+
+  console.log(TrendsArray);
+
   return (
     <div>
       <h1>trends</h1>
